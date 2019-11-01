92a97fcf549cc06d6e5c0dfc6d41eddb8baa6867
Jeronimo <jerokilian@hotmail.com>
2019-11-01 12:13:41 -0300

    arregle cosas by gon

---
+++ programacion.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+  fetch("https://api.themoviedb.org/3/tv/popular?api_key=310e7e56f84e1bd28bcf90582689ab22&language=en-US&page=1")
+  .then(function(r){
+    return r.json()
+  })
+  .then(function(data){
+    console.log(data);
+  })
+  .catch(function(e){
+    console.log(e);
+  })
+}
