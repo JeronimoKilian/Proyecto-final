079784391d8b77d9f2e3da070a0d0e4ffc22a93b
Jeronimo <jerokilian@hotmail.com>
2019-11-19 18:24:19 -0300

    detalle js
    Cambio el punto 4,y unos linkls

--- js/detalle.js
+++ js/detalle.js
@@ -27,7 +27,24 @@ window.addEventListener('load',function(){
     console.log(resultado.homepage)
     console.log(resultado.status)
     console.log(resultado.vote_average)
-    document.querySelector('.sinopsis').innerHTML=resultado.overview
+    var li = ''
+    var prepath = 'https://image.tmdb.org/t/p/original'
+    var ul = document.querySelector(".poster").src = prepath + resultado.poster_path
+
+      li += '<img src="'+prepath+ resultado.poster_path+ ' alt="">'
+      console.log(li);
+      document.querySelector(".tituloserie").innerHTML=resultado.name
+      document.querySelector('.sinopsis').innerHTML=resultado.overview
+      document.querySelector('.estreno').innerHTML=resultado.first_air_date
+      document.querySelector('.language').innerHTML=resultado.original_language
+      for (var i = 0; i < resultado.genres.length; i++) {
+        document.querySelector(".generos ul").innerHTML+= "<li>" + resultado.genres[i].name + "</li>"
+
+      }
+
+
+
+
   })
 
   fetch ("https://api.themoviedb.org/3/tv/" + idDeSerie + "/recommendations?api_key=64473b4750029f7eee1095d5f01e52e7&language=en-US&page=1" )
@@ -82,3 +99,45 @@ window.addEventListener('load',function(){
 
 })
 //recomendaciones
+
+
+//Favoritos
+var recuperoStorage = localStorage.getItem("seriesfavoritas");
+
+// Si todavía no tenía gifs favoritos
+if (recuperoStorage == null) {
+  // Creo una lista vacia
+  seriesfavoritas = [];
+} else {
+  // Descomprimo el TEXTO que tenia en storage en el array que necesito trabajar
+  seriesfavoritas = JSON.parse(recuperoStorage);
+}
+
+var datos = new URLSearchParams(location.search);
+var seriesfavoritas = datos.get("seriesfavoritas");
+
+if (gifsFavoritos.includes(idGif)) {
+  document.querySelector("button").innerHTML = "QUITAR DE FAVORITOS";
+}
+document.querySelector("button").onclick = function() {
+
+
+  //Paso 2: Modificar la informacion
+  // Si el gif ya era favorito
+  if (gifsFavoritos.includes(idGif)) {
+    // Lo quito
+    var index = gifsFavoritos.indexOf(idGif);
+    gifsFavoritos.splice(index, 1);
+    document.querySelector("button").innerHTML = "AGREGAR FAVORITO";
+  } else {
+    //Lo agrego
+    gifsFavoritos.push(idGif);
+    document.querySelector("button").innerHTML = "QUITAR DE FAVORITOS";
+  }
+
+
+  //Paso 3: Escribir en storage
+  var infoParaStorage = JSON.stringify(gifsFavoritos);
+  localStorage.setItem("gifsFavoritos", infoParaStorage);
+  console.log(localStorage);
+}
